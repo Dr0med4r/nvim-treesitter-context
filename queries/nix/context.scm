69db2665cdfaed69808f4a65b75d1c0909f43dfb
Dr0med4r <trompeter-stoll@gmx.de>
2023-04-21 10:11:37 +0200

    adding nix languaqe context

---
+++ queries/nix/context.scm
@@ -0,0 +1,17 @@
+(function_expression 
+  body: (_) @context.end
+) @context
+
+
+(binding
+  expression: (_) @context.end
+) @context
+
+(apply_expression
+    argument: (_) @context.end
+) @context
+
+(variable_expression
+    argument: (_) @context.end
+) @context
+
