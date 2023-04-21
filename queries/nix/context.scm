3696941b5f8957d5774804998b8931aa4ddf8f67
Dr0med4r <trompeter-stoll@gmx.de>
2023-04-21 13:36:39 +0200

    nix context.scm

--- queries/nix/context.scm
+++ queries/nix/context.scm
@@ -1,17 +1,22 @@
-(function_expression 
-  body: (_) @context.end
-) @context
-
-
-(binding
-  expression: (_) @context.end
-) @context
-
-(apply_expression
-    argument: (_) @context.end
-) @context
-
-(variable_expression
-    argument: (_) @context.end
-) @context
-
+;; (function_expression 
+;;   body: (_) @context.end
+;; ) @context
+;; 
+;; (binding
+;;   expression: (_) @context.end
+;; ) @context
+;; 
+;; (apply_expression
+;;     argument: (_) @context.end
+;; ) @context
+;; 
+;; (variable_expression
+;;     argument: (_) @context.end
+;; ) @context
+;; 
+[
+ (function_expression)
+ (binding)
+ (apply_expression)
+ (variable_expression)
+ ] @context
